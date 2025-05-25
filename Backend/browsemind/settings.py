ecf83b8dca0ad553882373eff5ded25f71250f9d
soroosh.esmaeilian <soroosh.esmaeilian@wedgenetworks.com>
2025-05-25 16:12:09 -0600

    ..

--- Backend/browsemind/settings.py
+++ Backend/browsemind/settings.py
@@ -137,6 +137,11 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'OPTIONS',  # Ensure OPTIONS is allowed
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = False
